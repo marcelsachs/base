d0f1ee4b9faeb75bf7f0250c0aef379907f529f6
marcelsachs <sachsmarcel@proton.me>
2026-09-19 17:38:32 +0200

    nvidia: drop the amdgpu blacklist comment

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -9,7 +9,6 @@
     nvidiaPersistenced = true;
     package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
-  # Raphael iGPU: both monitors are on the RTX; keep amdgpu out of DRM/WebGL.
   boot.blacklistedKernelModules = [ "amdgpu" ];
   boot.kernelParams = [ "module_blacklist=amdgpu" ];
 }
